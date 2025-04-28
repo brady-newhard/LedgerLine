5a4a9cbea65bda90a7e5cde56adb02b19d360b86
Tanzie <tanziehickory@gmail.com>
2025-04-27 19:37:19 -0700

    css. migrations

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
-from .views import unlocked_modes_view, TransactionList, TransactionCreate
+from .views import (
+    unlocked_modes_view, 
+    TransactionList, 
+    TransactionCreate,
+    CategoryList,
+    CategoryCreate,
+    CategoryUpdate,
+    CategoryDelete
+)
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,4 +18,9 @@ urlpatterns = [
     path('transactions/create/', TransactionCreate.as_view(), name='transaction_create'),
     path('register/', views.register, name='register'),
     
+    # Category URLs
+    path('categories/', CategoryList.as_view(), name='category_list'),
+    path('categories/create/', CategoryCreate.as_view(), name='category_create'),
+    path('categories/<int:pk>/update/', CategoryUpdate.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', CategoryDelete.as_view(), name='category_delete'),
 ]
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /home/robwistrand/code/ga/projects/LedgerLine/venv
+setenv VIRTUAL_ENV /Users/tanziehickory/code/ga/labs/LedgerLine/venv
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
