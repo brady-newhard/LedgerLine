37da11fb3fc0df3081187b842c58a6900c93ede3
User Name <user@email.com>
2025-04-27 22:31:13 -0400

    Feat: Admin, and Forms

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ModeUnlock, Transaction
+from .models import ModeUnlock, Transaction, Budget
 
 admin.site.register(ModeUnlock)
 admin.site.register(Transaction)
+admin.site.register(Budget)
 
--- main_app/forms.py
+++ main_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Budget, BudgetItem
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -14,4 +15,21 @@ class UserRegistrationForm(UserCreationForm):
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
-        return user 
+        return user
+
+class BudgetForm(forms.ModelForm):
+    class Meta:
+        model = Budget
+        fields = ['budget_name', 'budgeting_type', 'amount', 'start_date', 'end_date']
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+class BudgetItemForm(forms.ModelForm):
+    class Meta:
+        model = BudgetItem
+        fields = ['name', 'amount']
+        widgets = {
+            'amount': forms.NumberInput(attrs={'step': '0.01', 'min': '0'}),
+        } 
