20b52d58451ab5e4dc453b3fed4d388e7926a1c7
Brady Newhard <brady.russell@gmail.com>
2025-05-08 23:12:56 -0400

    Create Superuser

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 
-from .models import ModeUnlock, Transaction, Category, Budget, Calendar
+from .models import (
+    ModeUnlock, Transaction, Category, Budget, Calendar,
+    BudgetItem, Income, ModeHistory, CriticalSpending,
+    EssentialBill, SurvivalExpenseSchedule, CategorySpendingLimit,
+    SavingsGoal, FreedomFundPlan, FreedomExpense, SavingsContribution,
+    StabilityRatioTarget
+)
 
 admin.site.register(ModeUnlock)
 admin.site.register(Transaction)
 admin.site.register(Category)
 admin.site.register(Budget)
 admin.site.register(Calendar)
+admin.site.register(BudgetItem)
+admin.site.register(Income)
+admin.site.register(ModeHistory)
+admin.site.register(CriticalSpending)
+admin.site.register(EssentialBill)
+admin.site.register(SurvivalExpenseSchedule)
+admin.site.register(CategorySpendingLimit)
+admin.site.register(SavingsGoal)
+admin.site.register(FreedomFundPlan)
+admin.site.register(FreedomExpense)
+admin.site.register(SavingsContribution)
+admin.site.register(StabilityRatioTarget)
 
 
